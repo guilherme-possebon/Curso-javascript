ce6d9c6cac2740ebd4591404a657192414b9d5b7
Guilherme Possebon <gpossebon67@gmail.com>
2023-02-27 20:10:45 -0300

    Corrigindo um bug no script
    Bug fix no "verificador" de horas

--- Exercicios/Site da hora e do dia/script.js
+++ Exercicios/Site da hora e do dia/script.js
@@ -2,18 +2,18 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var horas = data
-    msg.innerHTML = `Agora são ${horas} horas.`
+    var horas = data.toLocaleTimeString();
+    
     if (horas > 0 && horas < 12) {
-        msg.innerHTML = `Agora são ${horas} horas. Bom dia`
+        msg.innerHTML = `Agora são ${horas} horas. Bom dia!`
         img.src = 'fotomanha.png'
         document.body.style.background = "#ffcca1"
     } else if (horas >= 12 && horas < 18) {
-        msg.innerHTML = `Agora são ${horas} horas. Boa tarde`
+        msg.innerHTML = `Agora são ${horas} horas. Boa tarde!`
         img.src = 'fototarde.png'
         document.body.style.background = "#b93e19"
     } else {
-        msg.innerHTML = `Agora são ${horas} horas. Boa noite`
+        msg.innerHTML = `Agora são ${horas} horas. Boa noite!`
         img.src = 'fotonoite.png'
         document.body.style.background = "#131720"
     }
